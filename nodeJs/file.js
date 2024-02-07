c3a837727c773d7c36f7bf0f7089209d75a2df44
Jaidev Singh Malik <jaidevsinghmalik@Jaidevs-MacBook-Air.local>
2024-02-07 22:54:35 +0100

    Learn creating file using fs

--- nodeJs/file.js
+++ nodeJs/file.js
@@ -1,4 +1,10 @@
+// Creating file using fs
 const fs = require("fs")
 
 // Synchronous
-fs.writeFileSync("vanshika.txt", "Jai Shree Ram")
+// fs.writeFileSync("vanshika.txt", "Jai Shree Ram")
+
+// File creatiion 2nd way
+
+// Asynchronous
+fs.writeFile("himanshu.txt", "Sita Ram", (err) => {})
